804207610a81f7e854e831744557643fd098494f
Brandon Simmons <brandon.m.simmons@gmail.com>
2014-02-13 18:08:53 -0500

    Experiments w/r/t the ABA problem and pointer equality

--- TestAtomics.hs
+++ TestAtomics.hs
@@ -1,18 +1,29 @@
 import Control.Concurrent
 import Data.Atomics.Counter
+import Data.Atomics
+import Data.IORef
 import Control.Monad
 import GHC.Conc
 import Control.Exception(evaluate)
 
-
 main = do
-    n0 <- test newCounter readCounter incrCounter
-    n1 <- test newMVar takeMVar (\n v-> modifyMVar_ v (evaluate . (+1)) )
+    testCAS_ABA_0
+    testCAS_ABA_0_modAfterAtomicMod
+    testCAS_ABA_0_modBefAtomicMod
+    testCAS_ABA_1
+    testCAS_ABA_2
+    testCAS_ABA_3
+
+    counterTest
+
+counterTest = do
+    n0 <- testAtomicCount newCounter readCounter incrCounter
+    n1 <- testAtomicCount newMVar takeMVar (\n v-> modifyMVar_ v (evaluate . (+1)) )
     if n0 /= n1
         then putStrLn $ "Counter broken: expecting "++(show n1)++" got "++(show n0)
         else putStrLn "OK"
 
-test new read incr = do
+testAtomicCount new read incr = do
   let n = 1000000
   procs <- getNumCapabilities
 
@@ -22,3 +33,114 @@ test new read incr = do
   mapM_ (\v-> putMVar v ()) starts ; mapM_ (\v-> takeMVar v) dones
   
   read counter
+
+-- test ABA issue with these three cases:
+--   ()
+--   Bool
+--       {-# NOINLINE True #-}
+--   let true = True
+
+
+
+-- returns False
+testCAS_ABA_0 = do
+    a <- newIORef ()
+    ta <- readForCAS a
+    atomicModifyIORef' a (\u-> (u,u))
+    (res, _) <- casIORef a ta () 
+    print res
+{- same
+testCAS_ABA_0_nonstrict = do
+    a <- newIORef ()
+    ta <- readForCAS a
+    atomicModifyIORef a (\u-> (u,u))
+    (res, _) <- casIORef a ta () 
+    print res
+testCAS_ABA_0_u = do
+    a <- newIORef ()
+    ta <- readForCAS a
+    atomicModifyIORef' a (const ((),()))
+    (res, _) <- casIORef a ta () 
+    print res
+testCAS_ABA_0_sameu = do
+    let {-# NOINLINE u #-} 
+        u = ()
+    a <- newIORef u
+    ta <- readForCAS a
+    atomicModifyIORef' a (const (u,u))
+    (res, _) <- casIORef a ta () 
+    print res
+-}
+
+-- returns True
+testCAS_ABA_1 = do
+    a <- newIORef ()
+    ta <- readForCAS a
+    modifyIORef a (const ())   -- i.e. readIORef >> writeIORef
+    (res, _) <- casIORef a ta () 
+    print res
+
+{- same
+testCAS_ABA_1_casMod = do
+    a <- newIORef ()
+    ta <- readForCAS a
+    atomicModifyIORefCAS_ a id
+    (res, _) <- casIORef a ta () 
+    print res
+testCAS_ABA_1_id = do
+    a <- newIORef ()
+    ta <- readForCAS a
+    modifyIORef a id   -- i.e. readIORef >> writeIORef
+    (res, _) <- casIORef a ta () 
+    print res
+-}
+
+-- returns True
+-- ... so the issue isn't re-ordering of readForCas and the read in modifyIORef
+-- in fact, no combination of the barriers provided seem to work.
+testCAS_ABA_2 = do
+    a <- newIORef ()
+    ta <- readForCAS a
+    loadLoadBarrier
+    modifyIORef a (const ())   -- i.e. readIORef >> writeIORef
+    (res, _) <- casIORef a ta () 
+    print res
+
+testCAS_ABA_3 = do
+    barrier <- newIORef ()
+
+    a <- newIORef ()
+    ta <- readForCAS a
+
+    atomicModifyIORef' barrier (\u-> (u,u))  -- just a barrier
+    modifyIORef a (const ())   -- i.e. readIORef >> writeIORef
+    atomicModifyIORef' barrier (\u-> (u,u))  -- just a barrier
+    
+    (res, _) <- casIORef a ta () 
+    print res
+
+-- INTERESTING!: /adding/ the modifyIORef /after/ the atomicModifyIORef causes this to return True!
+testCAS_ABA_0_modAfterAtomicMod = do
+    barrier <- newIORef ()
+
+    a <- newIORef ()
+    ta <- readForCAS a
+
+    atomicModifyIORef' a (\u-> (u,u))
+    modifyIORef a (const ())   -- i.e. readIORef >> writeIORef
+    
+    (res, _) <- casIORef a ta () 
+    print res
+
+-- ...whereas this one returns False again
+testCAS_ABA_0_modBefAtomicMod = do
+    barrier <- newIORef ()
+
+    a <- newIORef ()
+    ta <- readForCAS a
+
+    modifyIORef a (const ())   -- i.e. readIORef >> writeIORef
+    atomicModifyIORef' a (\u-> (u,u))
+    
+    (res, _) <- casIORef a ta () 
+    print res
